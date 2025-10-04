05aabeafd52bc4b0fcf0242d66bb919d7bc08012
adesug <adesugiantoro89@gmail.com>
2025-10-04 12:04:59 +0700

    configuration setup

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export const STATUS = {
+  TODO: 'To Do',
+  INPROGRESS: 'In Progress',
+  ONRIVIEW: 'On Review',
+  DONE: 'Done',
+  UNKNOWN: 'Unknown'
+}
+
+export const PRIORITY = {
+  LOW: 'Low',
+  MEDIUM: 'Medium',
+  HIGH: 'High',
+  URGENT: 'Urgent',
+  UNKNOWN: 'Unknown'
+}
